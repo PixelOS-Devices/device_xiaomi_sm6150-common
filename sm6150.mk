d429e61a2c66a44961064807d19d64861e1cfd5d
PIPIPIG233666 <pig.priv@gmail.com>
2019-09-14 21:46:19 -0400

    sm6150-common: audio: Rename audio_tuning_mixer_tavil.txt to audio_tuning_mixer.txt
     * Silence the following:
       ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
       MCS-RT-CTL: Can't open the configuration file /vendor/etc/audio_tuning_mixer.txt.
    
    Change-Id: I959588613a61c57b417e93a38757281fb178f586

--- sm6150.mk
+++ sm6150.mk
@@ -63,7 +63,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt
+    $(LOCAL_PATH)/configs/audio/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
