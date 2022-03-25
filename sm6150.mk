33a1194aacf825d9164bcc7e87808df03fe58e38
basamaryan <basam.aryan@gmail.com>
2022-03-24 23:42:54 -0400

    sm6150-common: Welcome PixelOS
    Change-Id: I83835698764cbf5b63f63c5597915d41ffad8c53

--- sm6150.mk
+++ sm6150.mk
@@ -277,7 +277,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
